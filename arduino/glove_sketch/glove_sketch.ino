42c2e9260bfa26717314fb377d56a8d1ce0e45db
Nathan Thompson <Nathan.Mike.Thompson@gmail.com>
2018-02-03 16:14:09 -0700

    Adding initial Arduino sketch

---
+++ arduino/glove_sketch/glove_sketch.ino
@@ -0,0 +1,13 @@
+const int palmPin = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(100);
+  int val = analogRead(palmPin);
+  Serial.println(val);
+}
