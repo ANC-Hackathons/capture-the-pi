ddb365f7992d1f339c66d39ed115c3a120622ac3
Nathan Thompson <nathan.mike.thompson@gmail.com>
2018-02-10 21:27:22 +0000

    Add initial base selection buttons; set GPIO pin based on selection

--- pi/base.py
+++ pi/base.py
@@ -2,24 +2,38 @@
 # -*- coding: iso-8859-1 -*-
 
 import RPi.GPIO as GPIO
+import Tkinter
 import time
+
+class app_tk(Tkinter.Tk):
+  def __init__(self, parent):
+    Tkinter.Tk.__init__(self,parent)
+    self.parent = parent
+    self.initialize()
+
+  def initialize(self):
+    self.grid()
+
+    buttonRed = Tkinter.Button(self, text=u"Red Base", command=self.setRed)
+    buttonRed.grid(column=0, row=0);
+
+    buttonGreen = Tkinter.Button(self, text=u"Green Base", command=self.setGreen)
+    buttonGreen.grid(column=1, row=0);
+
+  def setRed(self):
+    print 'Setting RED'
+    GPIO.output(PIN_1, GPIO.LOW);
+
+  def setGreen(self):
+    print 'Setting GREEN'
+    GPIO.output(PIN_1, GPIO.HIGH);
 
 if __name__ == "__main__":
   PIN_1 = 26
-  PIN_2 = 19
   GPIO.setmode(GPIO.BCM)
-  GPIO.setup(PIN_1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-  GPIO.setup(PIN_2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+  GPIO.setup(PIN_1, GPIO.OUT)
 
-  while True:
-    if GPIO.input(PIN_1):
-      print 'target 1 input was HIGH'
-    else:
-      print 'target 1 input was LOW'
 
-    if GPIO.input(PIN_2):
-      print 'target 2 input was HIGH'
-    else:
-      print 'target 2 input was LOW'
-
-    time.sleep(0.200)
+  app = app_tk(None)
+  app.title('Capture The Pi')
+  app.mainloop()
