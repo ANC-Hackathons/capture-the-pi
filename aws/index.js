5f28d21999eddc58ce52ddfa077dea66370f4269
Nathan Thompson <Nathan.Mike.Thompson@gmail.com>
2018-03-11 00:34:09 -0700

    Implement more of API

--- aws/index.js
+++ aws/index.js
@@ -1,8 +1,41 @@
-var express = require('express')
-var app = express()
+let express = require('express')
+let app = express()
+
+let blue= 0;
+let red= 0;
 
 app.get('/', function (req, res) {
     res.send('Hello World')
 })
+
+app.get('/score', function(req, res) {
+  res.json({ red, blue });
+});
+
+app.get('/score/red', function(req, res) {
+  res.json({ red });
+});
+
+app.get('/score/blue', function(req, res) {
+  res.json({ blue });
+});
+
+app.put('/score/red/:score', function(req, res) {
+  red += req.params.score;
+  console.log(`Red score now: ${red}`);
+}
+
+app.put('/score/blue/:score', function(req, res) {
+  blue += req.params.score;
+  console.log(`Blue score now: ${blue}`);
+}
+
+app.post('/reset', function(req, res) {
+  red = 0;
+  blue = 0;
+  console.log('Reset occurred');
+  console.log(`Red score now: ${red}`);
+  console.log(`Blue score now: ${blue}`);
+}
 
 app.listen(3000)
