382541d29c7f01b321c58e589dd2eecf8c863028
Nathan Thompson <Nathan.Mike.Thompson@gmail.com>
2018-03-11 00:35:00 -0700

    Fix missing )

--- aws/index.js
+++ aws/index.js
@@ -23,12 +23,12 @@ app.get('/score/blue', function(req, res) {
 app.put('/score/red/:score', function(req, res) {
   red += req.params.score;
   console.log(`Red score now: ${red}`);
-}
+});
 
 app.put('/score/blue/:score', function(req, res) {
   blue += req.params.score;
   console.log(`Blue score now: ${blue}`);
-}
+});
 
 app.post('/reset', function(req, res) {
   red = 0;
@@ -36,6 +36,6 @@ app.post('/reset', function(req, res) {
   console.log('Reset occurred');
   console.log(`Red score now: ${red}`);
   console.log(`Blue score now: ${blue}`);
-}
+});
 
 app.listen(3000)
