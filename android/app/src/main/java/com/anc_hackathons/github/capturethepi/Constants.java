0a50b3e2d9c7b44f05d7514dbbc09a7445e4433a
Nathan Thompson <Nathan.Mike.Thompson@gmail.com>
2018-03-13 17:18:14 -0600

    Send score to Pebble when received on phone, register callback to handle messages from Pebble to phone

---
+++ android/app/src/main/java/com/anc_hackathons/github/capturethepi/Constants.java
@@ -0,0 +1,15 @@
+package com.anc_hackathons.github.capturethepi;
+
+import java.util.UUID;
+
+/**
+ * Created by nathanthompson on 3/12/18.
+ */
+
+public class Constants {
+    public static final UUID PEBBLE_SIFTER_UUID = UUID.fromString("1dd0f46a-5253-413a-b2e9-80827fc0a039");
+
+    public static final int RED_SCORE = 0x0;
+    public static final int BLUE_SCORE = 0x1;
+    public static final int NEW_KILL = 0x2;
+}
--- android/app/src/main/java/com/anc_hackathons/github/capturethepi/MainActivity.java
+++ android/app/src/main/java/com/anc_hackathons/github/capturethepi/MainActivity.java
@@ -1,11 +1,14 @@
 package com.anc_hackathons.github.capturethepi;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import com.anc_hackathons.github.capturethepi.data.model.Score;
 import com.anc_hackathons.github.capturethepi.data.remote.ScoreService;
+import com.getpebble.android.kit.PebbleKit;
+import com.getpebble.android.kit.util.PebbleDictionary;
 
 import java.io.IOException;
 
@@ -44,6 +47,22 @@ public class MainActivity extends AppCompatActivity {
         getScore();
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // Register the receiver
+        PebbleKit.registerReceivedDataHandler(getApplicationContext(), dataReceiver);
+    }
+
+    @Override
+    public void onPause() {
+        super.onResume();
+
+        // Register the receiver
+        unregisterReceiver(dataReceiver);
+    }
+
     public void getScore() {
         scoreServer.getScore().enqueue(new Callback<Score>() {
             @Override
@@ -56,6 +75,15 @@ public class MainActivity extends AppCompatActivity {
                     if (redScore != response.body().getRed() || blueScore != response.body().getBlue()) {
                         redScore = response.body().getRed();
                         blueScore = response.body().getBlue();
+
+                        // Create a new dictionary
+                        PebbleDictionary dict = new PebbleDictionary();
+
+                        dict.addInt32(Constants.RED_SCORE, redScore);
+                        dict.addInt32(Constants.BLUE_SCORE, blueScore);
+
+                        // Send the dictionary
+                        PebbleKit.sendDataToPebble(getApplicationContext(), Constants.PEBBLE_SIFTER_UUID, dict);
                     }
                 }else {
                     int statusCode  = response.code();
@@ -74,4 +102,35 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    // Create a new receiver to get AppMessages from the C app
+    PebbleKit.PebbleDataReceiver dataReceiver = new PebbleKit.PebbleDataReceiver(Constants.PEBBLE_SIFTER_UUID) {
+
+        @Override
+        public void receiveData(Context context, int transaction_id,
+                                PebbleDictionary dict) {
+            // A new AppMessage was received, tell Pebble
+            PebbleKit.sendAckToPebble(context, transaction_id);
+
+            // If the tuple is present...
+            Long redScoreValue = dict.getInteger(Constants.RED_SCORE);
+            if(redScoreValue != null) {
+                // Read the integer value
+                int red = redScoreValue.intValue();
+            }
+
+            Long blueScoreValue = dict.getInteger(Constants.BLUE_SCORE);
+            if(blueScoreValue != null) {
+                // Read the integer value
+                int blue = blueScoreValue.intValue();
+            }
+
+            Long newKill = dict.getInteger(Constants.NEW_KILL);
+            if(newKill != null) {
+                // Read the integer value
+                int kill = newKill.intValue();
+            }
+        }
+
+    };
 }
